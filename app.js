9216b9758457e42cc0baf61d1cd4263a2c6e5809
Carl Whittaker <carl359@gmail.com>
2015-08-12 12:36:06 +0100

    Plug into Slack

--- app.js
+++ app.js
@@ -1,28 +1,51 @@
 import { SerialPort } from 'serialport';
-import connect from 'connect';
-import http from 'http';
+import request from 'request';
+
+const SLACK_HOOK_URL = 'https://slack.com/api/chat.postMessage';
+const SLACK_CHANNEL = process.env.SLACK_CHANNEL;
+const SLACK_TOKEN = process.env.SLACK_TOKEN;
+const SLACK_USERNAME = process.env.SLACK_USERNAME;
+const THRESHOLD = 1000;
+
+function sendMessage(channel, text, cb) {
+  const form = { channel, text, as_user: true, token: SLACK_TOKEN, username: 'caffeinator' };
+  request.post(SLACK_HOOK_URL, { form }, (err, res, body) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`Sent: "${channel}" "${text}"`, body);
+    }
+  });
+}
+
+function notifyCoffeeReady(){
+  sendMessage(SLACK_CHANNEL, '@here coffee is ready!')
+}
+
+function notifyCoffeeStarted(){
+  sendMessage(SLACK_CHANNEL, 'a fresh pot is brewing');
+}
 
 const serialPort = new SerialPort(process.env.SERIAL_PORT, {
   baudrate: 9600,
 });
 
-let reading = 0;
+let reading = 0,
+  lastReading;
 
 serialPort.on('open', () => {
   serialPort.on('data', data => {
-    console.log(data.toString());
-    reading = parseInt(data.toString(), 10);
+    reading = parseFloat(data.toString());
+
+    console.log(`Reading: ${reading}`);
+    if (lastReading > THRESHOLD && reading < THRESHOLD) {
+      notifyCoffeeReady();
+    }
+
+    if (lastReading < THRESHOLD && reading > THRESHOLD) {
+      notifyCoffeeStarted();
+    }
+
+    lastReading = reading;
   });
 });
-
-const app = connect()
-.use((req, res) => {
-  const body = {
-    brewing: reading > 1000,
-  };
-
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify(body));
-});
-
-http.createServer(app).listen(process.env.HTTP_PORT);
